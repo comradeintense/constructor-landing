5407b5c17ed57d3285f7bc7abf459e896aa33d66
comradeintense <in10se_SL@yahoo.com>
2023-08-27 19:32:21 +0300

    slider

---
+++ src/data/recenzii.js
@@ -0,0 +1,15 @@
+const recenzii = [
+	{
+		titlu: 'Serviciu exceptional',
+		recenzie:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab numquam consequatur sit perferendis sapiente consectetur pariatur. Maxime odit facere nesciunt asperiores, nulla nostrum natus. Placeat adipisci accusamus similique suscipit dolore.',
+		nume: 'Izabela Marioara',
+	},
+	{
+		titlu: 'Fantastic nu imi vine a crede',
+		recenzie: 'Trolollol. Placeat adipisci accusamus similique suscipit dolore.',
+		nume: 'Patrunjica Fericita',
+	},
+];
+
+export default recenzii;
