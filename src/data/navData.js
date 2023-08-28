322c2b26ef8c9d069282db46a25266f3adc52866
comradeintense <in10se_SL@yahoo.com>
2023-08-29 00:32:42 +0300

    added echipa

---
+++ src/data/navData.js
@@ -0,0 +1,24 @@
+const navData = [
+	{
+		name: 'Acasă',
+		path: '#',
+	},
+	{
+		name: 'Despre Noi',
+		path: '#',
+	},
+	{
+		name: 'Servicii',
+		path: '#',
+	},
+	{
+		name: 'Proiecte',
+		path: '#',
+	},
+	{
+		name: 'Echipa noastră',
+		path: '#',
+	},
+];
+
+export default navData;
