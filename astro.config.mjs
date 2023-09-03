4a215cf432e94fde71b406bbaaaa643a3347628c
comradeintense <in10se_SL@yahoo.com>
2023-09-03 15:15:37 +0300

    modified astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-	outDir: './dist',
-	base: '/uidaily/constructor',
-	site: 'https://intenseui.dev',
+
 	integrations: [tailwind()],
 });
